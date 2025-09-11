6b3891a73fe099293b203dba1e1163712f901810
Sean R. Abraham <abraham@burningman.org>
2025-09-11 11:25:49 -0400

    improve some admin pages UX (#412)

--- web/template/admintypes_templ.go
+++ web/template/admintypes_templ.go
@@ -79,7 +79,7 @@ func AdminTypes(deployment, versionName, versionRef string) templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 3, "<h1 id=\"doc-title\">Edit Incident Types</h1>")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 3, "<h1 id=\"doc-title\">Incident Types</h1>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}

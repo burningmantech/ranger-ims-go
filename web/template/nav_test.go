417508389356318d28cb486d9d6fec08b51d306e
Sean R. Abraham <abraham@burningman.org>
2026-08-03 13:59:56 -0400

    feat(nav): have IMS logo link to table for entity in scope (#738)
    e.g. if you're viewing a Field Report, clicking the logo will take you
    to the Field Reports table for the event in scope.
    
    This also adds some fun emoji to the items in the username dropdown,
    because why not?

---
+++ web/template/nav_test.go
@@ -0,0 +1,64 @@
+//
+// See the file COPYRIGHT for copyright information.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+package template_test
+
+import (
+	"regexp"
+	"strings"
+	"testing"
+
+	"github.com/burningmantech/ranger-ims-go/web/template"
+	"github.com/stretchr/testify/require"
+)
+
+func TestNavLogoLink(t *testing.T) {
+	t.Parallel()
+
+	logoLink := func(t *testing.T, eventName, eventSection string) string {
+		t.Helper()
+		var sb strings.Builder
+		require.NoError(t, template.Nav(eventName, eventSection).Render(t.Context(), &sb))
+		match := regexp.MustCompile(`<a id="ims-logo"[^>]*href="([^"]*)"`).FindStringSubmatch(sb.String())
+		require.NotNil(t, match, "no ims-logo link found")
+		return match[1]
+	}
+
+	t.Run("no event", func(t *testing.T) {
+		t.Parallel()
+		require.Equal(t, "/ims/app", logoLink(t, "", ""))
+	})
+
+	t.Run("event, no section", func(t *testing.T) {
+		t.Parallel()
+		require.Equal(t, "/ims/app/events/2026", logoLink(t, "2026", ""))
+	})
+
+	t.Run("incidents", func(t *testing.T) {
+		t.Parallel()
+		require.Equal(t, "/ims/app/events/2026/incidents", logoLink(t, "2026", "incidents"))
+	})
+
+	t.Run("field reports", func(t *testing.T) {
+		t.Parallel()
+		require.Equal(t, "/ims/app/events/2026/field_reports", logoLink(t, "2026", "field_reports"))
+	})
+
+	t.Run("visits", func(t *testing.T) {
+		t.Parallel()
+		require.Equal(t, "/ims/app/events/2026/visits", logoLink(t, "2026", "visits"))
+	})
+}
